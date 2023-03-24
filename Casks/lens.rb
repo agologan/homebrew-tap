c647061bbfb525d3e277fc2b9310da220fd9012c
Alexandru Gologan <alexandru.gologan@gmail.com>
2023-03-24 13:59:53 +0200

    fix duplicate version

--- Casks/lens.rb
+++ Casks/lens.rb
@@ -1,7 +1,6 @@
 cask "lens" do
   arch arm: "-arm64"
 
-  version "6.4.6"
   version "6.4.10"
   sha256 arm:   "c9bb27ca7cb31d7ac9195aef4074b9b88746ebec8e242ae8b7ec9cfa092ed112",
          intel: "096324f60a72ad71b44ec16b180fd3f2965755f17610b2296054a456f86da8a9"
