6e195919e858e5c092614df28a7a50c6b1f6038c
Alexandru Gologan <1990924+agologan@users.noreply.github.com>
2023-05-04 08:23:11 +0000

    sops: add 3.7.4 bottle.

--- Formula/sops.rb
+++ Formula/sops.rb
@@ -6,6 +6,14 @@ class Sops < Formula
   license "MPL-2.0"
   head "https://github.com/agologan/sops.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/agologan/homebrew-tap/releases/download/sops-3.7.4"
+    sha256 cellar: :any_skip_relocation, ventura:      "51df84d9101cf643fd7e99f2fe57270191c66d0be06cd00686a8df758cb5f735"
+    sha256 cellar: :any_skip_relocation, monterey:     "3ca33b08a5bb66646dfb65cfaacf620705c7976eb5e4143a9e2657a1ba6b9fe1"
+    sha256 cellar: :any_skip_relocation, big_sur:      "4326c924f8cc92562b2feb76c5861913195cbe8453583b26d5af2d84423e9d69"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "014f9550cdde07a83495a97dcef28133963a082066087d416d78d2d6206d34d7"
+  end
+
   depends_on "homebrew/core/go" => :build
 
   def install
