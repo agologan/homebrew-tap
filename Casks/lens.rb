f9534a2a9e459f33057fca1ae07bd4bfd5ce07d9
Alexandru Gologan <alexandru.gologan@gmail.com>
2023-06-23 17:24:33 +0300

    use new 6.5.2 lens build

--- Casks/lens.rb
+++ Casks/lens.rb
@@ -2,8 +2,8 @@ cask "lens" do
   arch arm: "-arm64"
 
   version "6.5.2"
-  sha256 arm:   "417663242666bc52f37e0b6e08724404162f12904c02a7cf5fa9807c9528e164",
-         intel: "7ad006e327e3fa5d2220f78bc98663ffbfe1534dc6f93371ac31caa9c393ad4a"
+  sha256 arm:   "0a64bf42b18cb2ca71a39e7b36b25410dfdc062500fbace4e3ac50e96b66974e",
+         intel: "fee06c828f21330c769fe09aea46ded59c51890f00b6400733d96e9543f4dad2"
 
   url "https://github.com/agologan/lens/releases/download/v#{version}/OpenLens-#{version}#{arch}.dmg"
   name "Lens"
