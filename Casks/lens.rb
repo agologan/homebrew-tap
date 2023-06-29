1fe976e973fe506522880613a9d1ccc263330f6a
Alexandru Gologan <agologan@users.noreply.github.com>
2023-06-29 02:23:01 +0000

    bump lens to 6.5.2

--- Casks/lens.rb
+++ Casks/lens.rb
@@ -1,9 +1,9 @@
 cask "lens" do
   arch arm: "-arm64"
 
-  version "null"
-  sha256 arm:   "Not",
-         intel: "Not"
+  version "6.5.2"
+  sha256 arm:   "0a64bf42b18cb2ca71a39e7b36b25410dfdc062500fbace4e3ac50e96b66974e",
+         intel: "fee06c828f21330c769fe09aea46ded59c51890f00b6400733d96e9543f4dad2"
 
   url "https://github.com/agologan/lens/releases/download/v#{version}/OpenLens-#{version}#{arch}.dmg"
   name "Lens"
