72081bdd98cd0feed6ad799e883e853ef3d71251
Alexandru Gologan <alexandru.gologan@gmail.com>
2025-10-30 13:17:58 +0200

    remove unused code and formulas and fix update script

--- update.sh
+++ update.sh
@@ -3,21 +3,24 @@
 # shellcheck disable=SC2312
 set -eu
 
-CURRENT=$(grep 'version ".*"' Casks/lens.rb | tr -s ' ' | cut -d ' ' -f3 | tr -d '"')
-LATEST=$(curl -sL https://api.github.com/repos/agologan/lens/releases/latest | jq '.name' | tr -d 'v"')
+CASK="freelens"
+BINARY="Freelens"
+CURRENT=$(grep 'version ".*"' "Casks/${CASK}.rb" | tr -s ' ' | cut -d ' ' -f3 | tr -d '"')
+LATEST=$(curl -sL "https://api.github.com/repos/agologan/${CASK}/releases/latest" | jq '.name' | tr -d 'v"')
 
-if [[ "${CURRENT}" != "${LATEST}" ]]; then
-    echo "Updating lens to v${LATEST}"
-    INTEL_SHA=$(curl -sL "https://github.com/agologan/lens/releases/download/v${LATEST}/OpenLens-${LATEST}.dmg.sha256" | cut -d ' ' -f1 )
-    ARM_SHA=$(curl -sL "https://github.com/agologan/lens/releases/download/v${LATEST}/OpenLens-${LATEST}-arm64.dmg.sha256" | cut -d ' ' -f1 )
-    echo $INTEL_SHA
-    echo $ARM_SHA
-    export INTEL_SHA
-    export ARM_SHA
-    export VERSION="${LATEST}"
-    envsubst < Casks/lens.rb.tpl > Casks/lens.rb
-    git add Casks/lens.rb
-    git commit -m "bump lens to ${LATEST}"
+if [[ "${CURRENT}" != "${LATEST}" ]]
+then
+  echo "Updating ${CASK} to v${LATEST}"
+  INTEL_SHA=$(curl -sL "https://github.com/agologan/${CASK}/releases/download/v${LATEST}/${BINARY}-${LATEST}-macos-amd64.dmg.sha256" | cut -d ' ' -f1)
+  ARM_SHA=$(curl -sL "https://github.com/agologan/${CASK}/releases/download/v${LATEST}/${BINARY}-${LATEST}-macos-arm64.dmg.sha256" | cut -d ' ' -f1)
+  echo "${INTEL_SHA}"
+  echo "${ARM_SHA}"
+  export INTEL_SHA
+  export ARM_SHA
+  export VERSION="${LATEST}"
+  envsubst <"Casks/${CASK}.rb.tpl" >"Casks/${CASK}.rb"
+  git add Casks/"${CASK}".rb
+  git commit -m "bump ${CASK} to ${LATEST}"
 else
-    echo 'Already up to date.'
+  echo 'Already up to date.'
 fi
