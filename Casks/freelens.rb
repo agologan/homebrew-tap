78dce840cd5e8f31308c173a8ec52970cbe06265
Alexandru Gologan <alexandru.gologan@gmail.com>
2026-05-14 13:56:18 +0300

    bump freelens to 1.9.0-blue

--- Casks/freelens.rb
+++ Casks/freelens.rb
@@ -1,16 +1,14 @@
 cask "freelens" do
   arch arm: "arm64", intel: "amd64"
 
-  version "1.8.0-blue"
-  sha256 arm:   "a9932a52e22d4621b2366fa6272ee0cf0c36618721fd5e63d923994ed13a3685",
-         intel: "37119fc84b62380c8071fbee4c866b0f3a9e9c3f897ef3730b5e45a00956c327"
+  version "1.9.0-blue"
+  sha256 arm:   "4c541a34234e89291d034988554ec7e636840d8f83985bc0611e81ac2b4306be",
+         intel: "188825c3beecccdf6d4f6f8fdd12c4a3f993975a205ac93053536ae500d13c4a"
 
   url "https://github.com/agologan/freelens/releases/download/v#{version}/Freelens-#{version}-macos-#{arch}.dmg"
   name "Freelens"
   desc "Kubernetes IDE"
   homepage "https://github.com/agologan/freelens/"
-
-  depends_on macos: ">= :big_sur"
 
   app "Freelens.app"
 
